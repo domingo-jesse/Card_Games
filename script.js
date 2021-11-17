1443cfc96e86357ee2b2fa71982b76223afb19cd
darkends 1 <39747952+domingo-jesse@users.noreply.github.com>
2021-11-17 04:16:04 +0000

    started to build deck for card game

---
+++ script.js
@@ -0,0 +1,39 @@
+import Deck from './deck.js'
+
+const computerCardSlot = document.querySelector('.computer-card-slot')
+
+const playerCardSlot = document.querySelector('.player-card-slot')
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+const text = document.querySelector('.text')
+
+let playerDeck, computerDeck
+
+startGame() 
+function startGame() {
+  const deck = new Deck()
+  deck.shuffle()
+  
+
+  const deckMidpoint  = Math.ceil(deck.numberOfCards / 2)
+
+  playerDeck =  new Deck(deck.cards.slice(0, deckMidpoint))
+  computerDeck =  new Deck(deck.cards.slice( deckMidpoint, deck.numberOfCards))
+
+  cleanBeforeRound()
+
+}
+
+function cleanBeforeRound() {
+  computerCardSlot.innerHTML = ""
+  playerCardSlot.innerHTML = ""
+  text.innerText = ""
+  
+  updateDeckCount() 
+} 
+
+
+function updateDeckCount() {
+  computerDeckElement.innerText = computerDeck.numberOfCards
+  playerDeckElement.innerText = playerDeck.numberOfCards
+}
